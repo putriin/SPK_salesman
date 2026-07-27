8f1287a7ba679189b154319f6e264fd9ae1ce97b
putriin <yagustibismillah@gmail.com>
2026-07-27 16:06:34 +0700

    ubah judul navbar perhitungan Topsis to perankingan

--- app/Views/layout/dashboard.php
+++ app/Views/layout/dashboard.php
@@ -99,7 +99,7 @@
 
                     <a class="nav-link <?= url_is('perhitungan*') ? 'active' : '' ?>"
                         href="<?= base_url('perhitungan') ?>">
-                        Perhitungan TOPSIS
+                        Perankingan
                     </a>
 
                     <a class="nav-link <?= url_is('cetak*') ? 'active' : '' ?>" href="<?= base_url('cetak') ?>">
